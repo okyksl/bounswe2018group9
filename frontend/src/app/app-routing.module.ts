50042ec921df389c94991197027098ac0210874f
Serkan Özel <serkan.ozel@boun.edu.tr>
2018-11-04 22:27:18 +0300

    feat(profile): add initial profile page module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthModule' },
-  { path: 'feed', loadChildren: './events/events.module#EventsModule' }
+  { path: 'feed', loadChildren: './events/events.module#EventsModule' },
+  { path: 'profile', loadChildren: './profile/profile.module#ProfileModule'}
 ];
 
 @NgModule({
